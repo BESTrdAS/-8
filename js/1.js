fa3ed48fdd641bad2e0f144b559bb992a743ae8a
BESTrdAS <897843079@qq.com>
2018-12-19 15:36:06 +0800

    add

--- js/1.js
+++ js/1.js
@@ -11,11 +11,11 @@ var boxBoundRight=boxX+boxWidth-ballRadius;
 var boxBoundTop=boxY+ballRadius;
 var boxBoundBottom=boxY+boxHeight-ballRadius;
 
-var ballX=0;
-var ballY=0;
+var ballX=50;
+var ballY=50;
 
-var ballDx=10;
-var ballDy=10;
+var ballDx=2;
+var ballDy=2;
 
 var ctx;
 
@@ -27,24 +27,14 @@ var grad;
 function init(){
 	ctx=document.getElementById("canvas").getContext("2d");
 	ctx.lineWidth=ballRadius;
-	
-	grad=ctx.createLinearGradient(boxX,boxY,boxX+boxWidth,boxY+boxHeight);
-	
 	 
-	ctx.fillStyle=grad;
 	moveball();
 	setInterval(moveball,50);
 }
+
+
 function moveball(){
 	ctx.clearRect(boxX,boxY,boxWidth,boxHeight);
-	
-	ctx.fillRect(boxX,boxY,ballRadius,boxHeight);
-	
-	ctx.fillRect(boxBoundRight,boxY,ballRadius,boxHeight);
-	
-	ctx.fillRect(boxX,boxY,boxWidth,ballRadius);
-	
-	ctx.fillRect(boxX,boxBoundBottom,boxWidth,ballRadius);
 	
 	boundCheck();
 	
@@ -69,18 +59,36 @@ var key= [
 			"autumn1.jpg","autumn2.jpg","autumn3.jpg","autumn4.jpg",
 			"winter1.jpg","winter2.jpg","winter3.jpg","winter4.jpg"
 ];
+var arrMusic=[
+			"1.mp3","2.mp3","3.mp3","4.mp3","5.mp3","6.mp3","7.mp3","8.mp3","9.mp3","10.mp3",
+			"11.mp3","12.mp3","13.mp3","14.mp3","15.mp3","16.mp3","17.mp3","18.mp3","19.mp3","20.mp3",
+			"21.mp3","22.mp3","23.mp3","24.mp3","25.mp3","26.mp3","27.mp3","28.mp3","29.mp3","30.mp3",
+			"31.mp3","32.mp3","33.mp3","34.mp3","35.mp3","36.mp3","37.mp3","38.mp3","39.mp3","40.mp3",
+			"41.mp3","42.mp3","43.mp3","44.mp3","45.mp3","46.mp3","47.mp3","48.mp3","49.mp3","50.mp3",
+			"51.mp3","52.mp3","53.mp3","54.mp3","55.mp3","56.mp3","57.mp3","58.mp3","59.mp3","60.mp3",
+			"61.mp3","62.mp3","63.mp3","64.mp3","65.mp3","66.mp3","67.mp3","68.mp3","69.mp3","70.mp3",
+			"71.mp3","72.mp3","73.mp3","74.mp3","75.mp3","76.mp3","77.mp3","78.mp3","79.mp3","80.mp3",
+			"81.mp3","82.mp3","83.mp3","84.mp3","85.mp3","86.mp3","87.mp3","88.mp3"
+]
+
+var countMus=0;
+var curMusic=document.getElementById("mp3");
+
 
 function boundCheck(){
 	var tmpBallX=ballX+ballDx;
 	var tmpBallY=ballY+ballDy;
 	
+	countMus++;
+	if(countMus>88)
+		countMus=1;
 	if(tmpBallX<boxBoundLeft){
 		ballDx=-ballDx;
 		tmpBallX=boxBoundLeft;
 		countSpr++;
 		countSpr%=4;
 		document.body.style.background="url(img/"+key[countSpr]+")";
-		document.body.style.backgroundSize="100% 1200px"
+		document.body.style.backgroundSize="100% 1200px";
 	}
 	
 	if(tmpBallX>boxBoundRight){
@@ -111,6 +119,8 @@ function boundCheck(){
 	}
 	ballX=tmpBallX;
 	ballY=tmpBallY;
+	//curMusic.src=arrMusic[countMus];
+	//curMusic.play();
 }
 
 function change(){
@@ -118,3 +128,4 @@ function change(){
 	ballDy=Number(document.getElementById("vv").value);
 	return false;
 }
+
